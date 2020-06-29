3f2dea36b66c6f69c95b7a4bfff10be21444108f
farid.najafov <farid.najafov@outlook.com>
2020-06-29 21:15:54 +0400

    -- some unfinished changes

--- src/main/java/com/xe/controller/RatesController.java
+++ src/main/java/com/xe/controller/RatesController.java
@@ -1,12 +1,14 @@
 package com.xe.controller;
 
+import com.xe.entity.XCurrency;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -14,6 +16,8 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/rates")
 public class RatesController {
+
+
 
     /**
      * http://localhost:8081/rates
@@ -23,5 +27,10 @@ public class RatesController {
         HttpSession session = httpServletRequest.getSession(false);
         log.info("GET -> /rates");
         return session == null ? "redirect:/login" : "rates";
+    }
+
+    @PostMapping
+    public String exchange() {
+        return "rates";
     }
 }
