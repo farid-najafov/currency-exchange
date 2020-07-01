706d4283a3984b81fdee1ea28ddc360fd9582040
Samir Allahverdi <59360227+SamirAllahverdi@users.noreply.github.com>
2020-07-01 09:15:02 +0400

    Ss (#23)
    * --- parsing backend side added. front is unfinished.
    
    * --- parsing backend side added. front is unfinished.
    
    Co-authored-by: azsena1997 <azsena2020>

--- src/main/java/com/xe/entity/Exchange2.java
+++ src/main/java/com/xe/entity/Exchange2.java
@@ -4,9 +4,11 @@ package com.xe.entity;
 import com.xe.util.XCurrency;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Exchange2 {
     XCurrency baseCcy;
     XCurrency quoteCcy;
