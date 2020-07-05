26181850df563cc44c99667c320ef21e7fdba987
farid-najafov <65233889+farid-najafov@users.noreply.github.com>
2020-07-05 15:35:59 +0400

    -- working on main-page-authorized (#32)

--- src/main/resources/static/js/date-select.js
+++ src/main/resources/static/js/date-select.js
@@ -23,23 +23,42 @@ $("#date-to").datepicker({
   },
   onSelect: function () {
     const dateObject = $(this).datepicker("getDate");
-    const month = dateObject.toLocaleDateString("en-US", { month: "long" });
-    const day = dateObject.getDate();
+      const month = dateObject.toLocaleDateString("en-US", {month: "long"});
+      const day = dateObject.getDate();
 
-    $("#date-to").val("").attr("placeholder", "");
+      $("#date-to").val("").attr("placeholder", "");
 
-    $("#date-to-value").html(
-      `<span class="datepicker-value-date">${day}</span><span class="datepicker-value-month">${month}</span>`
-    );
+      $("#date-to-value").html(
+          `<span class="datepicker-value-date">${day}</span><span class="datepicker-value-month">${month}</span>`
+      );
   },
 });
-
+$("#date-from1")
+    .datepicker({
+        dateFormat: "dd MM yy",
+        beforeShow: function () {
+            $("#ui-datepicker-div")
+                .removeClass("datepicker-right")
+                .addClass("datepicker-left");
+        },
+        onSelect: function () {
+            const dateObject = $(this).datepicker("getDate");
+            const month = dateObject.toLocaleDateString("en-US", {month: "long"});
+            const day = dateObject.getDate();
+            const year = dateObject.getFullYear();
+            $("#date-from-value").html(
+                `<span class="datepicker-value-date">${day}</span><span class="datepicker-value-month">${month}</span>
+              <span class="datepicker-value-year">${year}</span>`
+            );
+        },
+    })
+    .position({my: "center"});
 window.addEventListener("DOMContentLoaded", () => {
-  const today = new Date();
-  $("#date-from-value, #date-to-value").html(
-    `<span class="datepicker-value-date">${today.getDate()}</span><span class="datepicker-value-month">${today.toLocaleDateString(
-      "en-US",
-      { month: "long" }
-    )}</span>`
-  );
+    const today = new Date();
+    $("#date-from-value, #date-to-value").html(
+        `<span class="datepicker-value-date">${today.getDate()}</span><span class="datepicker-value-month">${today.toLocaleDateString(
+            "en-US",
+            {month: "long"}
+        )}</span>`
+    );
 });
