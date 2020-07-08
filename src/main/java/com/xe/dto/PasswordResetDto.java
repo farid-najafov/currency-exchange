9278940d74c6f4d466ddf1eb15849f5e3ca4dd33
Aqil <41694337+Aqilzeka@users.noreply.github.com>
2020-07-08 21:55:06 +0400

    updated (#38)

--- src/main/java/com/xe/dto/PasswordResetDto.java
+++ src/main/java/com/xe/dto/PasswordResetDto.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
-@FieldMatch(first = "password", second = "confirmPassword", message = "Password fields must match")
+@FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match")
 public class PasswordResetDto {
 
     @NotNull(message = "Password cannot be null")
@@ -15,7 +15,9 @@ public class PasswordResetDto {
     private String password;
 
     @NotNull(message = "Password cannot be null")
-    @Size(min = 1, message = "must be greater than 3 digits")
+//    @Size(min = 3, message = "must be greater than 3 digits")
     private String confirmPassword;
+
+    @NotNull
     private String token;
 }
--- src/main/java/com/xe/validation/FieldMatch.java
+++ src/main/java/com/xe/validation/FieldMatch.java
@@ -1,26 +1,32 @@
 package com.xe.validation;
 
-import javax.validation.Constraint;
+
 import javax.validation.Payload;
-import java.lang.annotation.*;
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
 @Constraint(validatedBy = FieldMatchValidator.class)
-@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
-@Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface FieldMatch {
-
-	String message() default "";
-	Class<?>[] groups() default {};
-	Class<? extends Payload>[] payload() default {};
-	
-	String first();
+public @interface FieldMatch
+{
+    String message() default "{constraints.field-match}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String first();
     String second();
-    
-    @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
-    @Retention(RetentionPolicy.RUNTIME)
+
+    @Target({TYPE, ANNOTATION_TYPE})
+    @Retention(RUNTIME)
     @Documented
-    @interface List {
-    	FieldMatch[] value();
+    @interface List
+    {
+        FieldMatch[] value();
     }
 }
